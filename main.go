71ad4f0c3d60f8e96c5d7fe9321421f9d4cf6ada
Beyram <beyram.taglietti@gmail.com>
2024-09-13 21:56:09 +0200

    chore: code cleanup

--- main.go
+++ main.go
@@ -5,33 +5,5 @@ import (
 )
 
 func main() {
-
 	cmd.Execute()
-
-	// arguments, err := utils.GetArguments()
-
-	// if err != nil {
-	// 	fmt.Println("Error while getting arguments", err)
-	// 	return
-	// }
-
-	// if arguments.Debug {
-	// 	color.Yellow("Debug mode enabled")
-	// }
-	// if len(arguments.IgnoreList) > 0 {
-	// 	color.Cyan("Ignoring folders: %v", arguments.IgnoreList)
-	// }
-
-	// rootFolder, err := utils.CreateFilesMap(arguments.FolderPath, 0, arguments.IgnoreList)
-	// if err != nil {
-	// 	fmt.Println("Error while walking through the folder", err)
-	// 	return
-	// }
-
-	// if arguments.Debug {
-	// 	fmt.Println("Initial folder structure")
-	// 	rootFolder.PrintTree("")
-	// }
-
-	// rootFolder.WatchTree(arguments.FolderPath, arguments.Command, arguments.IgnoreList)
 }
